92a15dce3b94577dfcc99a7b1e1199eb90daf7d7
Lachy Schuamcher <lachy@tydewest.com>
2022-10-05 23:03:00 +1100

    init

--- Application.js
+++ Application.js
@@ -4,8 +4,36 @@ const app = express();
 
 app.use(express.json());
 
-app.use(DefaultRoutes);
+app.get("/", (req, res) => {
+    try {
+        response.status(200);
+        response.json({
+        Message: 'Hello, World!',
+        });
+        return;
+    } catch (error) {
+        response.status(500);
+        response.json({
+        Message: 'Houston, we have a problem',
+        });
+    }
+})
+
+app.get("*", (req, res) => {
+    try {
+        response.status(200);
+        response.json({
+        Message: 'Nothing Found!',
+        });
+        return;
+    } catch (error) {
+        response.status(500);
+        response.json({
+        Message: 'Houston, we have a problem',
+        });
+    }
+})
 
 app.listen(3005, () => {
-  console.log(`Server Is Running At http://127.0.0.1/3005. Use Ctrl + C To Stop The Server.`);
+    console.log(`Server Is Running At http://127.0.0.1/3005. Use Ctrl + C To Stop The Server.`);
 });
