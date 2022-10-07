5ef4bdcf8a6102b9a1ac5a12bd12d1bae9231507
Lachy Schuamcher <lachy@tydewest.com>
2022-10-07 12:40:53 +1100

    simple

--- Application.js
+++ Application.js
@@ -1,39 +1,43 @@
 const express = require('express');
-const DefaultRoutes = require('./Routes/DefaultRoute.js');
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", (request, response) => {
     try {
         response.status(200);
-        response.json({
-        Message: 'Hello, World!',
-        });
+        response.json([{
+            Status: '200 - OK',
+            Message: 'Hello, World!',
+        }]);
         return;
     } catch (error) {
         response.status(500);
-        response.json({
-        Message: 'Houston, we have a problem',
-        });
+        response.json([{
+            Status: '500 - Internal Server Error',
+            Message: 'Houston, we have a problem',
+        }]);
     }
 })
 
-app.get("*", (req, res) => {
+app.get("*", (request, response) => {
     try {
-        response.status(200);
-        response.json({
-        Message: 'Nothing Found!',
-        });
+        response.status(404);
+        response.json([{
+            Status: '404 - Not Found',
+            Message: 'The Requested Resource Could Not Be Found!',
+        }]);
         return;
     } catch (error) {
         response.status(500);
-        response.json({
-        Message: 'Houston, we have a problem',
-        });
+        response.json([{
+            Status: '500 - Internal Server Error',
+            Message: 'Houston, we have a problem',
+        }]);
     }
 })
 
 app.listen(3005, () => {
-    console.log(`Server Is Running At http://127.0.0.1/3005. Use Ctrl + C To Stop The Server.`);
+    console.log(`Server Is Running At http://127.0.0.1:3005. Use Ctrl + C To Stop The Server.`);
 });
